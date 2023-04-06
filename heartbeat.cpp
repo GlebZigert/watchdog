45891d3457309227d010d05e3702c3587bb45051
gggg <ggg>
2023-04-06 16:28:59 +0300

    .

--- heartbeat.cpp
+++ heartbeat.cpp
@@ -1,4 +1,7 @@
 #include "HeartBeat"
+#include <QIODevice>
+#include <QFileInfo>
+#include <RunGuard.h>
 
 HeartBeat::HeartBeat(QObject *parent)
 
@@ -46,6 +49,54 @@ void HeartBeat::sockDisc()
 
 void HeartBeat::do_reload()
 {
+
+/*
+qDebug()<<"do_reload";
+
+    QString pathToExecutable = QString("/opt/Rif+/m_server");
+
+        // Check the path string was written correctly and Qt can see it
+        QFileInfo fileInfo(pathToExecutable);
+
+        QProcess *process = new QProcess(this);
+
+        if (fileInfo.exists())
+        {
+            if(fileInfo.isExecutable())
+            {
+                QObject::connect(process, &QProcess::errorOccurred,
+                    process, [&]()
+                {
+                    qWarning() << process->errorString();
+                });
+
+
+                QString executableDir = fileInfo.absolutePath();
+                process->setWorkingDirectory(executableDir);
+
+                QString absuluteExecutableFilePath = fileInfo.absoluteFilePath();
+
+
+
+                // If you need to check errors, use start(), but not startDetached()
+                qDebug()<<"absuluteExecutableFilePath "<<absuluteExecutableFilePath;
+                QProcess p;
+                p.start("pkill m_server");
+                p.waitForFinished();
+                process->start(absuluteExecutableFilePath, QStringList());
+              //  process.waitForFinished(1);
+            }
+            else
+            {
+                qWarning() << "Given file is not executable";
+            }
+        }
+        else
+        {
+            qWarning() << "File path does not exist or is set incorrectly";
+        }
+*/
+
     QProcess p;
     p.start("pkill m_server");
     p.waitForFinished();
@@ -53,16 +104,36 @@ void HeartBeat::do_reload()
 
     qDebug()<<"do_reload";
  //       QProcess::execute("/opt/Rif+/m_server > /home/master/1.txt");
-    QProcess *process = new QProcess(this);
+    delete(process);
+    process = new QProcess(this);
     QString file = QString("/opt/Rif+/m_server" );
-#if (defined (_WIN32) || defined (_WIN64))
-    // windows code
-    file.append(".exe");
-#endif
+
+process->setReadChannel(QProcess::StandardOutput);
+process->setProcessChannelMode(QProcess::ForwardedChannels);
 
 
+//process->setProcessChannelMode( QProcess::ForwardedOutputChannel );
+connect(process, SIGNAL(started()), this, SLOT(processStarted()));
+connect(process,SIGNAL(readyReadStandardOutput()),this,SLOT(readyReadStandardOutput()));
+connect(process, SIGNAL(finished(int)), this, SLOT(encodingFinished(int)));
 
-    process->start(file, QStringList());
+/*
+process->connect(process,&QProcess::readyReadStandardOutput,[=]{
+        qDebug()<<"new Data:";
+        qDebug().noquote()<<process->readAll();
+    });
+
+*/
+ //   connect( process, SIGNAL( finished( int, QProcess::ExitStatus ) ),
+ //            this, SLOT( onProgramFinished( int, QProcess::ExitStatus ) ), Qt::DirectConnection );
+
+process->start(file, QStringList(),QIODevice::ReadWrite);
+
+}
+
+void HeartBeat::read()
+{
+qDebug()<<"read";
 }
 
 void HeartBeat::incomingConnection(qintptr socketDescriptor)
@@ -75,4 +146,19 @@ void HeartBeat::incomingConnection(qintptr socketDescriptor)
 
     qDebug()<<socketDescriptor<<" Client connected";
     socket->write("{\"type\":\"connect\",\"status\":\"yes\"}");
+}
+
+void HeartBeat::processStarted()
+{
+    qDebug()<<"HeartBeat::processStarted()";
+}
+
+void HeartBeat::readyReadStandardOutput()
+{
+     qDebug()<<"HeartBeat::readyReadStandardOutput()";
+}
+
+void HeartBeat::encodingFinished(int)
+{
+      qDebug()<<"HeartBeat::encodingFinished(int)";
 }
--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <QCoreApplication>
 #include <HeartBeat>
+#include "RunGuard.h"
 int main(int argc, char *argv[])
 {
+
+    RunGuard guardCopyServer( "consoleWatcher" );
+    if ( !guardCopyServer.tryToRun() ) {
+
+        qDebug()<<"consoleWatcher уже запущен ";
+        guardCopyServer.release();
+        return 0;
+    }
+
+    RunGuard guardServer( "consoleWatcher" );
+    guardServer.tryToRun();
+
     QCoreApplication a(argc, argv);
     HeartBeat *heartBeat = new HeartBeat();
 
